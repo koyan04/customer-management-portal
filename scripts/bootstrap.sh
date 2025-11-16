6d56240b9bf1077cc96a1c3b2442242b3d68cefd
koyan04 <koyan.vpn@gmail.com>
2025-11-17 01:35:54 +0630

    chore(release): bump scripts to v1.0.19

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,7 +9,7 @@ set -euo pipefail
 
 OWNER="koyan04"
 REPO="customer-management-portal"
-TAG="v1.0.18"
+TAG="v1.0.19"
 
 INSTALLER_URL="https://raw.githubusercontent.com/${OWNER}/${REPO}/${TAG}/scripts/install.sh"
 
